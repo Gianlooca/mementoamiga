cc0d6c1fe90430944e2442eddea2eb4d21117953
Gianlooca Zimmatore <gianluca.zimmatore@gmail.com>
2024-02-29 10:46:46 +0000

    Add the selectors object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    const cards = document.querySelectorAll('.memory-card');
-    
-    function flipCard() {
-      this.classList.toggle('flip');
-    }
-    
-    for (var i = 0; i < cards.length; i++) {
-        cards[i].addEventListener('click', flipCard());
-      }
-    
-    });
+/**
+ * We created this object in order to access several DOM elements through JS
+ * (with the querySelector() method in an quicker way
+ */
+const selectors = {
+  mainCointaner: document.querySelector(".main-div"),
+  boardContainer: document.querySelector(".main"),
+  wrapperBoard: document.querySelector(".wrapper-board"),
+  board: document.querySelector(".board"),
+  moves: document.querySelector(".moves"),
+  timer: document.querySelector(".timer"),
+  start: document.querySelector("#start"),
+  win: document.querySelector(".win"),
+  controls: document.querySelector(".controls"),
+};
